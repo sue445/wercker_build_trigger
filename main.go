1996abea6e8ec93880edf66bf586af4525169b53
sue445 <sue445@sue445.net>
2017-09-24 15:38:57 +0900

    Migrate to sub command

--- main.go
+++ main.go
@@ -1,63 +1,30 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-)
-
-const (
-	DEFAULT_BRANCH = "master"
+	"github.com/mitchellh/cli"
+	"log"
+	"os"
 )
 
 var (
 	Version  string
 	Revision string
 )
-var configFile, token string
-var isPrintVersion bool
-
-func init() {
-	flag.StringVar(&configFile, "config", "", "Path to config file")
-	flag.StringVar(&token, "token", "", "API token")
-	flag.BoolVar(&isPrintVersion, "version", false, "Whether showing version")
-
-	flag.Parse()
-}
 
 func main() {
-	if isPrintVersion {
-		printVersion()
-		return
+	c := cli.NewCLI("werker_build_trigger", Version)
+	c.Args = os.Args[1:]
+
+	c.Commands = map[string]cli.CommandFactory{
+		"trigger": func() (cli.Command, error) {
+			return NewTrigger(), nil
+		},
 	}
 
-	if len(configFile) == 0 || len(token) == 0 {
-		flag.PrintDefaults()
-		return
-	}
-
-	config, err := LoadConfigFromFile(configFile)
-
+	exitStatus, err := c.Run()
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
-	wercker := NewWercker(token)
-
-	for _, run := range config.Pipelines {
-		if len(run.Branch) == 0 {
-			run.Branch = DEFAULT_BRANCH
-		}
-
-		ret, err := wercker.TriggerNewRun(run.Id, run.Branch)
-
-		if err == nil {
-			fmt.Printf("[%s:%s] Triggered run: %s\n", run.Id, run.Branch, ret.Url)
-		} else {
-			fmt.Printf("[%s:%s] Error: %v\n", run.Id, run.Branch, err)
-		}
-	}
-}
-
-func printVersion() {
-	fmt.Printf("zatsu_monitor v%s, build %s\n", Version, Revision)
+	os.Exit(exitStatus)
 }
