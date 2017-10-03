04e80cf9e15ee28814f9711089a64d1a2a66cfe1
sue445 <sue445@sue445.net>
2017-10-03 22:50:57 +0900

    Remove sub command

--- main.go
+++ main.go
@@ -1,33 +1,80 @@
 package main
 
 import (
-	"github.com/mitchellh/cli"
-	"log"
-	"os"
+	"flag"
+	"fmt"
+)
+
+const (
+	DEFAULT_BRANCH = "master"
 )
 
 var (
-	Version  string
-	Revision string
+	Version        string
+	Revision       string
+	configFile     string
+	token          string
+	isPrintVersion bool
 )
 
+func init() {
+	flag.StringVar(&configFile, "config", "", "Path to config file")
+	flag.StringVar(&token, "token", "", "API token")
+	flag.BoolVar(&isPrintVersion, "version", false, "Whether showing version")
+
+	flag.Parse()
+}
+
 func main() {
-	c := cli.NewCLI("werker_build_trigger", Version)
-	c.Args = os.Args[1:]
-
-	c.Commands = map[string]cli.CommandFactory{
-		"search": func() (cli.Command, error) {
-			return NewSearch(), nil
-		},
-		"trigger": func() (cli.Command, error) {
-			return NewTrigger(), nil
-		},
+	if isPrintVersion {
+		printVersion()
+		return
 	}
 
-	exitStatus, err := c.Run()
+	if len(configFile) == 0 || len(token) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	config, err := LoadConfigFromFile(configFile)
+
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
 
-	os.Exit(exitStatus)
+	wercker := NewWercker(token)
+
+	for _, configPipeline := range config.Pipelines {
+		if len(configPipeline.Branch) == 0 {
+			configPipeline.Branch = DEFAULT_BRANCH
+		}
+
+		if len(configPipeline.Id) > 0 {
+			ret, err := wercker.TriggerNewRun(configPipeline.Id, configPipeline.Branch)
+
+			if err == nil {
+				fmt.Printf("[pipelineId:%s][branch:%s] Triggered pipeline: %s\n", configPipeline.Id, configPipeline.Branch, ret.Url)
+			} else {
+				fmt.Printf("[pipelineId:%s][branch:%s] Error: %v\n", configPipeline.Id, configPipeline.Branch, err)
+			}
+
+		} else if len(configPipeline.Path) > 0 && len(configPipeline.Name) > 0 {
+			pipeline, err := wercker.FindPipeline(configPipeline.Path, configPipeline.Name)
+
+			if err != nil {
+				fmt.Printf("[path:%s][pipelineName:%s] Error: %v\n", configPipeline.Path, configPipeline.Name, err)
+			}
+
+			ret, err := wercker.TriggerNewRun(pipeline.Id, configPipeline.Branch)
+			if err == nil {
+				fmt.Printf("[path:%s][pipelineName:%s][pipelineId:%s][branch:%s] Triggered pipeline: %s\n", configPipeline.Path, configPipeline.Name, pipeline.Id, configPipeline.Branch, ret.Url)
+			} else {
+				fmt.Printf("[path:%s][pipelineName:%s][pipelineId:%s][branch:%s] Error: %v\n", configPipeline.Path, configPipeline.Name, pipeline.Id, configPipeline.Branch, err)
+			}
+		}
+	}
+}
+
+func printVersion() {
+	fmt.Printf("wercker_build_trigger v%s, build %s\n", Version, Revision)
 }
