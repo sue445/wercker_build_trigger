bf6261afc286aac276ea9f9a41e30c0741c647e8
sue445 <sue445@sue445.net>
2017-10-12 02:09:40 +0900

    [Resolved] exported method Wercker.TriggerNewRun should have comment or be unexported

--- wercker.go
+++ wercker.go
@@ -148,6 +148,7 @@ func (w *Wercker) FindPipeline(applicationPath string, pipelineName string) (pip
 	}
 }
 
+// TriggerNewRun trigger new run with specified pipeline id and branch
 func (w *Wercker) TriggerNewRun(pipelineId string, branch string) (run *WerckerRun, err error) {
 	currentTime := time.Now().Format("2006-01-02 15:04:05")
 	params := WerckerTriggerNewRunParam{
